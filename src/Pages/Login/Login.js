fd06ded823eab6c2b85d0efb7339180aea8171ed
iammahir2020 <1810659@iub.edu.bd>
2022-05-22 01:17:16 +0600

    social login done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,11 @@
 import React from "react";
+import SocialLogin from "../Shared/SocialLogin/SocialLogin";
 
 const Login = () => {
   return (
     <div>
       <h2>Login</h2>
+      <SocialLogin></SocialLogin>
     </div>
   );
 };
--- src/Pages/Shared/SocialLogin/SocialLogin.js
+++ src/Pages/Shared/SocialLogin/SocialLogin.js
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useLocation, useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faGoogle } from "@fortawesome/free-brands-svg-icons";
 
 const SocialLogin = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+  const handleGoogleSignin = () => {
+    signInWithGoogle();
+  };
+
+  let errorMessage;
+  let from = location.state?.from?.pathname || "/";
+
+  useEffect(() => {
+    if (user) {
+      console.log(user);
+      navigate(from, { replace: true });
+    }
+  }, [user, navigate, from]);
+
+  if (error) {
+    errorMessage = <p className="text-red-500">{error?.message}</p>;
+  }
+
   return (
     <div>
-      <h2>Social Login</h2>
+      {loading ? (
+        <button className="btn btn-outline text-white bg-gradient-to-r from-secondary to-primary w-full max-w-md mb-3 loading">
+          loading
+        </button>
+      ) : (
+        <button
+          onClick={handleGoogleSignin}
+          className="btn btn-outline btn-secondary w-full max-w-md mb-3 flex items-center justify-center gap-3"
+        >
+          <FontAwesomeIcon className="text-xl" icon={faGoogle} />{" "}
+          <p>Continue With Google</p>
+        </button>
+      )}
+
+      {errorMessage}
     </div>
   );
 };
